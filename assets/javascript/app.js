95d91e34215b6c71c2390e92604a681ba60f0cf3
Aijune <43250155+AFilippis@users.noreply.github.com>
2018-11-06 14:35:18 -0500

    Er...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,22 +4,30 @@ $(document).ready(function () {
     var maxRating = "PG";
 
     // Render button function.
-    function renderBtn() {
+    function renderBtns() {
         $('#btnContainer').empty();
         //Create buttons based on elements in array
         for (var i = 0; i < topics.length; i++) {
             var newBtn = $('<button>');
             newBtn.attr('data-name', topics[i]);
-            newBtn.attr('class', 'gif');
+            newBtn.addClass('gif');
+            newBtn.addClass('topicBtn')
+            newBtn.addClass('btn');
             newBtn.text(topics[i]);
             $('#btnContainer').append(newBtn);
         };
-    };
+        $(".topicBtn").unbind("click");
+        $(".topicBtn").on("click", function () {
+            $(".gifImage").unbind("click");
+            $("#gifContainer").empty();
 
+            populateGIFContainer($(this).text());
+        });
+    };
     function addBtn() {
         var input = $('#randomInput').val().trim();
         topics.push(input);
-        renderBtn();
+        renderBtns();
     };
 
     $('#submitBtn').click(function (event) {
@@ -28,6 +36,7 @@ $(document).ready(function () {
         console.log("added");
     });
 
+    
     function addGifsToContainer() {
         $.ajax({
             url: "https://api.giphy.com/v1/gifs/search?q=" + show +
@@ -35,68 +44,17 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             response.data.forEach(function (element) {
-                $(document).ready(function () {
-                    var topics = ["kittens", "otters", "dogs", "bunnies"];
-                    var numberOfGifs = 10;
-                    var maxRating = "PG";
-                
-                    // Render button function.
-                    function renderBtn() {
-                        $('#btnContainer').empty();
-                        //Create buttons based on elements in array
-                        for (var i = 0; i < topics.length; i++) {
-                            var newBtn = $('<button>');
-                            newBtn.attr('data-name', topics[i]);
-                            newBtn.attr('class', 'gif');
-                            newBtn.text(topics[i]);
-                            $('#btnContainer').append(newBtn);
-                        };
-                    };
-                
-                    function addBtn() {
-                        var input = $('#randomInput').val().trim();
-                        topics.push(input);
-                        renderBtn();
-                    };
-                
-                    $('#submitBtn').click(function (event) {
-                        event.preventDefault();
-                        addBtn();
-                        console.log("added");
-                    });
-                
-                    function addGifsToContainer() {
-                        $.ajax({
-                            url: "https://api.giphy.com/v1/gifs/search?q=" + show +
-                                "&api_key=Yzx8vjOJIoMqPNi0M0sKl3K8hbapqLEp&rating=" + cutOffRating + "&limit=" + numberOfGifs,
-                            method: "GET"
-                        }).then(function (response) {
-                            response.data.forEach(function (element) {
-                                newDiv = $("<div>");
-                                newDiv.addClass("individual-gif-container");
-                                newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
-                                var newImage = $("<img src = '" + element.images.url + "'>");
-                                newImage.addClass("gif-image");
-                                newDiv.append(newImage);
-                                $("#gif-container").append(newDiv);
-                            });
-                        });
-                    };
-                
-                    // Append input into btnContainer
-                    // By submit button or hitting enter
-                    // Call function to Gify API.
-                    // Limit gifs called
-                    // Display Gifs in gifContainer.
-                
-                    renderBtn();
-                    addGifsToContainer();
-                });
-                
-                // Yzx8vjOJIoMqPNi0M0sKl3K8hbapqLEp
+                newDiv = $("<div>");
+                newDiv.addClass("singularGifContainer");
+                newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+                var newImage = $("<img src = '" + element.images.url + "'>");
+                newImage.addClass("gifImage");
+                newDiv.append(newImage);
+                $("#gifContainer").append(newDiv);
             });
         });
     };
+
 
     // Append input into btnContainer
     // By submit button or hitting enter
@@ -104,7 +62,7 @@ $(document).ready(function () {
     // Limit gifs called
     // Display Gifs in gifContainer.
 
-    renderBtn();
+    renderBtns();
 
 });
 
